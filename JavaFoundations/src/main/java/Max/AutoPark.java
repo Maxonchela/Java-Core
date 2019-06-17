ebf1ee3a6c4ea5bde698c746c537f1a3023cea50
max <Maxonchela@mail.ru>
2019-06-17 20:18:28 +0700

    First commit

---
+++ JavaFoundations/src/main/java/Max/AutoPark.java
@@ -0,0 +1,18 @@
+package Max;
+
+public class AutoPark {
+    private Cars[] cars = new Cars[3];
+
+    public static void main(String[] args) {
+        AutoPark autoPark = new AutoPark();
+
+    }
+
+    private createCar(){
+
+    }
+
+
+
+
+}
