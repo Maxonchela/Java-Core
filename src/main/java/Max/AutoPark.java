aec7827c71abf9ebf5421aa0ba92179225260d6f
max <Maxonchela@mail.ru>
2019-06-18 12:48:52 +0700

    Second commit

---
+++ src/main/java/Max/AutoPark.java
@@ -0,0 +1,19 @@
+package Max;
+
+public class AutoPark {
+    private Cars[] cars = new Cars[3];
+
+    public static void main(String[] args) {
+        AutoPark autoPark = new AutoPark();
+
+
+    }
+
+//    private createCar(){
+//
+//    }
+
+
+
+
+}
