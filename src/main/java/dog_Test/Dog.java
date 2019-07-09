e8cca08073937ead0457ed19e9e29ea511552982
max <Maxonchela@mail.ru>
2019-07-09 22:42:42 +0300

    Try to commit it

---
+++ src/main/java/dog_Test/Dog.java
@@ -0,0 +1,43 @@
+package dog_Test;
+
+public class Dog{
+    public boolean testCollar;
+    public boolean testMuzzle;
+    public boolean testLeash;
+    public String name;
+
+    public Dog(String name){
+        this.name = name;
+    }
+
+    public void putCollar(){
+        System.out.println("Ошейник надет!");
+        this.testCollar = true;
+    }
+
+//    public void putCollar() {
+//
+//        System.out.println("Ошейник надет!");
+//        this.isCollarPutOn = true;
+//    }
+
+    public void putMuzzle(){
+        System.out.println("Намордник надет!");
+        this.testMuzzle = true;
+    }
+
+    public void takeLeash(){
+        System.out.println("Ошейник взяли!");
+        this.testLeash = true;
+    }
+
+    public void walk() throws Exception {
+        System.out.println("Going to walk?");
+        if(testLeash && testMuzzle && testCollar){
+            System.out.println("Everything OK - GO!");
+        }
+        else {
+            throw new Exception("Dog " + name + " don't ready for a walk");
+        }
+    }
+}
---
+++ src/main/java/dog_Test/Main.java
@@ -0,0 +1,17 @@
+package dog_Test;
+
+public class Main {
+    public static void main(String[] args) {
+       Dog dog = new Dog("Max");
+       dog.putCollar();
+       dog.putMuzzle();
+       dog.takeLeash();
+       try{
+           dog.walk();
+       }
+       catch (Exception e){
+           System.out.println(e.getMessage());
+           System.out.println("Проверяем экиперовку. Ошейник надет? " + dog.testCollar + " Поводок взяли? " + dog.testLeash + " Намордник надели? " + dog.testMuzzle);
+       }
+    }
+}
